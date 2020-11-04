4e43746e78ba9c16f6cbc3f362f90883703a27d4
xiongmao86 <xiongmao86dev@sina.com>
2020-11-04 15:48:28 +0800

    Initial.

---
+++ src/store.js
@@ -0,0 +1,24 @@
+
+pack_query_1 = {
+    "id": 0x7385,
+    
+    "response": false,
+    "opcode": 0x0000,
+    "truncated": false,
+    "recursion-disired": true,
+    "z": false,
+    "authentic-data": false,
+    "checking-disabled": false,
+
+    "query-count": 1,
+    "answer-count": 0,
+    "authority-count": 0,
+    "additional-information-count": 0
+
+    "query": {
+        "name": "collector.githubapp.com",
+        "type": "A",
+        "class": "IN"
+    }
+}
+
