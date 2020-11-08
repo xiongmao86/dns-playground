c50fb604f11217da7f51e0e9bff9109f1e7c4ec1
xiongmao86 <xiongmao86dev@sina.com>
2020-11-08 18:15:41 +0800

    First impression.

--- src/components/Packet.vue
+++ src/components/Packet.vue
@@ -26,31 +26,55 @@
                 <th colspan="16">ID</th>
             </tr>
             <tr>
-                <td colspan="16">{{pack.id}}</td>
+                <td colspan="16">{{`0x${pack.id.toString(16)}`}}</td>
             </tr>
             <tr>
-                <td>QR</td>
-                <td colspan="4">Opcode</td>
-                <td>AA</td>
-                <td>TC</td>
-                <td>RD</td>
-                <td>RA</td>
-                <td>Z</td>
-                <td>AD</td>
-                <td>CD</td>
-                <td colspan="4">RCODE</td>
+                <th>QR</th>
+                <th colspan="4">Opcode</th>
+                <th>AA</th>
+                <th>TC</th>
+                <th>RD</th>
+                <th>RA</th>
+                <th>Z</th>
+                <th>AD</th>
+                <th>CD</th>
+                <th colspan="4">RCODE</th>
             </tr>
             <tr>
-                <td colspan="16">QDCOUNT/ZOCOUNT</td>
+                <td>{{ pack.flags.response }}</td>
+                <td colspan="4">{{ pack.flags.opcode }}</td>
+                <td>{{ pack.flags.authoritative }}</td>
+                <td>{{ pack.flags.truncated }}</td>
+                <td>{{ pack.flags.recursion_desired }}</td>
+                <td>{{ pack.flags.recursion_available }}</td>
+                <td>{{ pack.flags.z }}</td>
+                <td>{{ pack.flags.authentic_data }}</td>
+                <td>{{ pack.flags.checking_disable }}</td>
+                <td colspan="4">{{ pack.flags.error_code }}</td>
             </tr>
             <tr>
-                <td colspan="16">ANCOUNT/PRCOUNT</td>
+                <th colspan="16">QDCOUNT/ZOCOUNT</th>
             </tr>
             <tr>
-                <td colspan="16">NSCOUNT/UPCOUNT</td>
+                <td colspan="16">{{ pack.query_count }}</td>
             </tr>
             <tr>
-                <td colspan="16">ARCOUNT</td>
+                <th colspan="16">ANCOUNT/PRCOUNT</th>
+            </tr>
+            <tr>
+                <td colspan="16">{{ pack.answer_count }}</td>
+            </tr>
+            <tr>
+                <th colspan="16">NSCOUNT/UPCOUNT</th>
+            </tr>
+            <tr>
+                <td colspan="16">{{ pack.authority_count }}</td>
+            </tr>
+            <tr>
+                <th colspan="16">ARCOUNT</th>
+            </tr>
+            <tr>
+                <td colspan="16">{{ pack.additional_information_count }}</td>
             </tr>
         </tbody>
         <tfoot>
@@ -88,7 +112,13 @@ export default {
 </script>
 
 <style>
+caption {
+    font-weight: bold;
+}
 td {
+    border: 1px solid lightsteelblue;
+}
+th {
     border: 1px solid lightsteelblue;
 }
 tfoot {
--- src/store/index.js
+++ src/store/index.js
@@ -7,21 +7,21 @@ export default new Vuex.Store({
   state: {
     "id": 0x867f,
     "flags": {
-      "response": true,
+      "response": 1,
       "opcode": 0x0000,
-      "authoritative": false,
-      "truncated": false,
-      "recursion-desired": true,
-      "recursion-available": true,
-      "z": false,
-      "authentic-data": false,
-      "checking-disable": false,
-      "error-code": 0x0000,
+      "authoritative": 0,
+      "truncated": 0,
+      "recursion_desired": 1,
+      "recursion_available": 1,
+      "z": 0,
+      "authentic_data": 0,
+      "checking_disable": 0,
+      "error_code": 0x0000,
     },
-    "query-count": 1,
-    "answer-count": 3,
-    "authority-count": 5,
-    "additional-information-count": 5,
+    "query_count": 1,
+    "answer_count": 3,
+    "authority_count": 5,
+    "additional_information_count": 5,
     
     "querys": [{
       "name": "www.baidu.com",
@@ -33,108 +33,108 @@ export default new Vuex.Store({
         "name": "www.baidu.com",
         "type": "CNAME",
         "class": "IN",
-        "time-to-live": 600,
-        "data-length": 15,
+        "time_to_live": 600,
+        "data_length": 15,
         "CNAME": "www.a.shifen.com"
       },
       {
         "name": "www.a.shifen.com",
         "type": "A",
         "class": "IN",
-        "time-to-live": 600,
-        "data-length": 4,
+        "time_to_live": 600,
+        "data_length": 4,
         "address": "14.215.177.38"
       },
       {
         "name": "www.a.shifen.com",
         "type": "A",
         "class": "IN",
-        "time-to-live": 600,
-        "data-length": 4,
+        "time_to_live": 600,
+        "data_length": 4,
         "address": "14.215.177.38"
       }
     ],
-    "Authoritative-nameservers": [
+    "Authoritative_nameservers": [
       {
         "name": "a.shifen.com",
         "type": "NS",
         "class": "IN",
-        "time-to-live": 205,
-        "data-length": 6,
-        "name-server": "ns3.a.shifen.com"
+        "time_to_live": 205,
+        "data_length": 6,
+        "name_server": "ns3.a.shifen.com"
       },
       {
         "name": "a.shifen.com",
         "type": "NS",
         "class": "IN",
-        "time-to-live": 205,
-        "data-length": 6,
-        "name-server": "ns4.a.shifen.com"
+        "time_to_live": 205,
+        "data_length": 6,
+        "name_server": "ns4.a.shifen.com"
       },
       {
         "name": "a.shifen.com",
         "type": "NS",
         "class": "IN",
-        "time-to-live": 205,
-        "data-length": 6,
-        "name-server": "ns2.a.shifen.com"
+        "time_to_live": 205,
+        "data_length": 6,
+        "name_server": "ns2.a.shifen.com"
       },
       {
         "name": "a.shifen.com",
         "type": "NS",
         "class": "IN",
-        "time-to-live": 205,
-        "data-length": 6,
-        "name-server": "ns1.a.shifen.com"
+        "time_to_live": 205,
+        "data_length": 6,
+        "name_server": "ns1.a.shifen.com"
       },
       {
         "name": "a.shifen.com",
         "type": "NS",
         "class": "IN",
-        "time-to-live": 205,
-        "data-length": 6,
-        "name-server": "ns5.a.shifen.com"
+        "time_to_live": 205,
+        "data_length": 6,
+        "name_server": "ns5.a.shifen.com"
       }
     ],
-    "additional-records": [
+    "additional_records": [
       {
         "name": "ns1.a.shifen.com",
         "type": "A",
         "class": "IN",
-        "time-to-live": 466,
-        "data-length": 4,
+        "time_to_live": 466,
+        "data_length": 4,
         "address": "61.135.165.224"
       },
       {
         "name": "ns2.a.shifen.com",
         "type": "A",
         "class": "IN",
-        "time-to-live": 497,
-        "data-length": 4,
+        "time_to_live": 497,
+        "data_length": 4,
         "address": "220.181.33.32"
       },
       {
         "name": "ns3.a.shifen.com",
         "type": "A",
         "class": "IN",
-        "time-to-live": 465,
-        "data-length": 4,
+        "time_to_live": 465,
+        "data_length": 4,
         "address": "112.80.255.253"
       },
       {
         "name": "ns4.a.shifen.com",
         "type": "A",
         "class": "IN",
-        "time-to-live": 138,
-        "data-length": 4,
+        "time_to_live": 138,
+        "data_length": 4,
         "address": "14.215.177.229"
       },
       {
         "name": "ns5.a.shifen.com",
         "type": "A",
         "class": "IN",
-        "time-to-live": 353,
-        "data-length": 4,
+        "time_to_live": 353,
+        "data_length": 4,
         "address": "180.76.76.95"
       }
     ]
